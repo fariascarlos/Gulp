5d12ad1a21a8a908cc73e3208295f05f4fa6bf88
jose carlos farias <zecarlos12121@hotmail>
2022-08-03 01:33:29 -0300

    execultando gulp no projeto

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -12,8 +12,15 @@ const image = require ('gulp-imagemin')
 
 function tarefasCSS(cb){
 
-    return gulp.src('./vendor/**/*.css')
-    .pipe(concat('libs.css'))
+    return gulp.src([
+        './vendor/**/*.css',
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+        './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+        './src/css/style.css'
+
+
+])
+    .pipe(concat('styles.css'))
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'})) // libs.min.css
     .pipe(gulp.dest('./dist/css'))
@@ -21,10 +28,17 @@ function tarefasCSS(cb){
 }
 
 function tarefasJS(){
-    return gulp.src('./vendor/**/*.js')
-    .pipe(concat('libs.js'))
+    return gulp.src([
+        './vendor/**/*.js',
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './node_modules/@fortawesome/fontawesome-free/js/fontawesome.js',
+        './src/js/custom.js'
+        
+    ])
+    .pipe(concat('scripts.js'))
     .pipe(uglify())
-    .pipe(rename({suffix: '.js'}))
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./dist/js'))
 
 }
